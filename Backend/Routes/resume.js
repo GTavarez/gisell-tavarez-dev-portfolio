ccd72703ac06af9cd03f48edecc5a5e13820f8d1
Gtavarez <giselltavarez@gmail.com>
2025-12-03 06:41:00 -0500

    Remove backend from frontend branch

--- Backend/Routes/resume.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { generateResumePDF } = require("../controllers/resume");
-
-router.get("/", generateResumePDF);
-
-module.exports = router;
